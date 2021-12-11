e997eeb2f470b5f948bf941b1b5a4d500d4748bf
Jay Patel <hardik19998@gmail.com>
2021-12-10 19:19:04 -0500

    Updated movies API

--- routes/movies.js
+++ routes/movies.js
@@ -13,6 +13,8 @@ router.post("/", async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
 
     const genre = await  Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send("Invalid genre.");
+
     let movie = new Movie({ 
         title: req.body.title,
         genre: {
@@ -26,5 +28,50 @@ router.post("/", async (req, res) => {
     movie = await movie.save();
     res.send(movie);
 });
+
+router.put("/:id", async (req, res) => {
+    const { error } = validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const genre = await  Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send("Invalid genre.");
+
+    const movie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          title: req.body.title,
+          genre: {
+            _id: genre._id,
+            name: genre.name
+          },
+          numberInStock: req.body.numberInStock,
+          dailyRentalRate: req.body.dailyRentalRate
+        },
+        { new: true }
+      );
+  
+    if (!movie)
+      return res.status(400).send("The genre with given Id does not exist.");
+  
+    res.send(movie);
+  });
+  
+  router.delete("/:id", async (req, res) => {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+  
+    if (!movie)
+      return res.status(400).send("The genre with given Id does not exist.");
+  
+    res.send(movie);
+  });
+  
+  router.get("/:id", async (req, res) => {
+    const movie = await Movie.findById(req.params.id);
+  
+    if (!movie)
+      return res.status(400).send("The genre with given Id does not exist.");
+  
+    res.send(movie);
+  });
 
 module.exports = router;
